5393fc6911d54e3fef852bee71aabc0bf9158339
sayan-chakraverty <34247964+sayan-chakraverty@users.noreply.github.com>
2017-12-07 02:17:32 +0530

    Update cancel-order.component.ts

--- src/app/cancel-order/cancel-order.component.ts
+++ src/app/cancel-order/cancel-order.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CancelOrderService } from './cancel-order.service';
 @Component({
   selector: 'app-cancel-order',
   templateUrl: './cancel-order.component.html',
   styleUrls: ['./cancel-order.component.css']
 })
 export class CancelOrderComponent implements OnInit {
-
-  constructor() { }
+  orderNumbers = [];
+  orderNumber = "--select--";
+  imageUrl: string = "../assets/images/pizza.jpg";
+  isCancelled=false;
+  isCancellationSuccess=false;
+  constructor(private cancelOrderService: CancelOrderService) { }
 
   ngOnInit() {
+    this.cancelOrderService.getOrderNumbers().subscribe(res => {
+      this.orderNumbers = res;
+    })  
   }
-
+  setOrderNumber(oNumber) {
+    this.orderNumber = oNumber;
+  }
+  cancelOrder(){
+    this.isCancelled=true;
+     this.cancelOrderService.cancelOrder(this.orderNumber).subscribe(order=>{
+          this.isCancellationSuccess=true;
+        });
+  }
 }
