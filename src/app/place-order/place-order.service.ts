7b7ce180f64d05dd35b1f3f4afeb92ced9261e51
sayan-chakraverty <34247964+sayan-chakraverty@users.noreply.github.com>
2017-12-07 21:39:29 +0530

    Update place-order.service.ts

--- src/app/place-order/place-order.service.ts
+++ src/app/place-order/place-order.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
-
+import { Http, Response, HttpModule, Headers, RequestOptions } from "@angular/http";
+import { Order } from './order';
+import { Observable } from 'rxjs';
 @Injectable()
 export class PlaceOrderService {
+  locations = [];
+  locationURL = "./assets/mockData/locationData.json";
+  placeOrderURL = "";
+  constructor(private http: Http) { }
+  getLocations() {
+    return this.http.get(this.locationURL).map((res: Response) => this.locations = res.json())
 
-  constructor() { }
+  }
 
+  placeOrder(order: Order) {
+    console.log(order);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(this.placeOrderURL, order, options)
+      .map(this.extractData)
+      .catch(this.handleErrorObservable);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || {};
+  }
+  private handleErrorObservable(error: Response | any) {
+    console.error(error.message || error);
+    return Observable.throw(error.message || error);
+  }
 }
