d5ca25d8509f6b47f91ae939bf24be8127b94817
sayan-chakraverty <34247964+sayan-chakraverty@users.noreply.github.com>
2017-12-08 02:07:37 +0530

    Update place-order.component.ts

--- src/app/place-order/place-order.component.ts
+++ src/app/place-order/place-order.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
 import { PlaceOrderService } from './place-order.service';
-import { Order } from './order';
-
+import { Order } from './order'
 @Component({
   selector: 'app-place-order',
   templateUrl: './place-order.component.html',
@@ -12,24 +11,42 @@ export class PlaceOrderComponent implements OnInit {
 
   imageUrl: string = "../assets/images/pizza.jpg";
   deliveryLocations = [];
+  shippingCosts = [];
   validOrder = false;
   orderSubmitted = false;
   mobile = "";
   deliveryLocation = "";
   order = new Order();
-  orderSuccess=false;
+  orderSuccess = false;
+  shippingCost = 0;
+  deliveryType = "Bike";
   constructor(private placeOrderService: PlaceOrderService) { }
 
   ngOnInit() {
-    // this.placeOrderService.getLocations().subscribe(res => {
-    //   this.deliveryLocations = res;
-    //   if (this.deliveryLocations.length > 0) {
-    //     this.deliveryLocation = this.deliveryLocations[0];
-    //   }
+    this.placeOrderService.getLocations().subscribe(res => {
+      this.deliveryLocations = res;
+      if (this.deliveryLocations.length > 0) {
+        this.deliveryLocation = this.deliveryLocations[0];
+      }
+    })
 
-    // })
+    this.placeOrderService.getShippingCosts().subscribe(res => {
+      this.shippingCosts = res;
+      this.calculateShippingCost();
+    })
+  }
+  changeDeliveryType(type) {
+    this.deliveryType = type;
+    this.calculateShippingCost();
   }
 
+   calculateShippingCost() {
+    this.shippingCosts.forEach(element => {
+      if (element.type == this.deliveryType) {
+        this.shippingCost = element.cost;
+      }
+    });
+  }
   setLocation(loc) {
     this.deliveryLocation = loc;
   }
@@ -39,12 +56,13 @@ export class PlaceOrderComponent implements OnInit {
     if (this.mobile != "" && this.mobile != undefined) {
       if (/^\d{10}$/.test(this.mobile)) {
         this.validOrder = true;
-        this.order.userName = "user1";
+        this.order.userName = sessionStorage.getItem('user');
         this.order.location = this.deliveryLocation;
         this.order.mobileNumber = this.mobile;
-        // this.placeOrderService.placeOrder(this.order).subscribe(order=>{
-        //   this.orderSuccess=true;
-        // });
+        this.order.deliveryType=this.deliveryType;
+        this.placeOrderService.placeOrder(this.order).subscribe(order => {
+          this.orderSuccess = true;
+        });
       }
     }
   }
