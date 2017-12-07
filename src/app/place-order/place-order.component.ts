67e62244cd4a3396ea97c19f40ba014aaae8e1d9
anubhab07 <34179359+anubhab07@users.noreply.github.com>
2017-12-08 02:26:24 +0530

    merge conflict resolved

--- src/app/place-order/place-order.component.ts
+++ src/app/place-order/place-order.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
 import { PlaceOrderService } from './place-order.service';
-<<<<<<< HEAD
-import { Order } from './order';
-import {MapLocationService} from "../map/map-location.service";
-import {FetchUserService} from "../login/fetch-user.service"
-
-=======
 import { Order } from './order'
->>>>>>> d5ca25d8509f6b47f91ae939bf24be8127b94817
 @Component({
   selector: 'app-place-order',
   templateUrl: './place-order.component.html',
@@ -24,14 +17,9 @@ export class PlaceOrderComponent implements OnInit {
   mobile = "";
   deliveryLocation = "";
   order = new Order();
-<<<<<<< HEAD
-  orderSuccess=false;
-  
-=======
   orderSuccess = false;
   shippingCost = 0;
   deliveryType = "Bike";
->>>>>>> d5ca25d8509f6b47f91ae939bf24be8127b94817
   constructor(private placeOrderService: PlaceOrderService) { }
 
   ngOnInit() {
@@ -40,10 +28,6 @@ export class PlaceOrderComponent implements OnInit {
       if (this.deliveryLocations.length > 0) {
         this.deliveryLocation = this.deliveryLocations[0];
       }
-<<<<<<< HEAD
-
-    })
-=======
     })
 
     this.placeOrderService.getShippingCosts().subscribe(res => {
@@ -54,7 +38,6 @@ export class PlaceOrderComponent implements OnInit {
   changeDeliveryType(type) {
     this.deliveryType = type;
     this.calculateShippingCost();
->>>>>>> d5ca25d8509f6b47f91ae939bf24be8127b94817
   }
 
    calculateShippingCost() {
@@ -76,14 +59,9 @@ export class PlaceOrderComponent implements OnInit {
         this.order.userName = sessionStorage.getItem('user');
         this.order.location = this.deliveryLocation;
         this.order.mobileNumber = this.mobile;
-<<<<<<< HEAD
-        this.placeOrderService.placeOrder(this.order).subscribe(order=>{
-          this.orderSuccess=true;
-=======
         this.order.deliveryType=this.deliveryType;
         this.placeOrderService.placeOrder(this.order).subscribe(order => {
           this.orderSuccess = true;
->>>>>>> d5ca25d8509f6b47f91ae939bf24be8127b94817
         });
       }
     }
